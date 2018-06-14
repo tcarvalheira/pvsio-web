ec6717884c291c33968c526f6e2ed5fb3bd3fc62
Tiago Carvalheira <=>
2018-06-14 16:21:23 +0100

    fix bug on top definition when page is scrolled down and buttons wasn't positioned over html buttons

--- src/client/app/widgets/container/Pagination.js
+++ src/client/app/widgets/container/Pagination.js
@@ -37,7 +37,7 @@ define(function (require, exports, module) {
     var WidgetEVO = require("widgets/core/WidgetEVO");
 
     const ButtonEVO = require("widgets/core/ButtonEVO")
-    
+
     /**
      * @function <a name="Pagination">Pagination</a>
      * @description Constructor.
@@ -69,7 +69,7 @@ define(function (require, exports, module) {
         opt.overflow = "hidden";
         this.coords = coords
         this.plainParent = opt.parent || 'body'
-        this.parent =(opt.parent) ? (`#${opt.parent}`) : 'body'
+        this.parent = (opt.parent) ? (`#${opt.parent}`) : 'body'
         this.previousButton = opt.previousButton !== undefined ? opt.previousButton : true
         this.nextButton = opt.nextButton !== undefined ? opt.nextButton : true
         this.useIcons = opt.useIcons !== undefined ? opt.useIcons : true
@@ -78,8 +78,8 @@ define(function (require, exports, module) {
         this.alignment = opt.alignment || 'left'
         this.callback = opt.callback || ((id) => id)
         this.div = d3.select(this.parent)
-        
-        
+
+
         this.buttons = []
 
         // invoke WidgetEVO constructor to create the widget
@@ -101,12 +101,12 @@ define(function (require, exports, module) {
      * @instance
      */
     Pagination.prototype.render = function (state, opt) {
-        
+
         /*** remove buttons */
         this.buttons.forEach((button) => {
             button.button.remove()
         })
-        this.buttons=[]
+        this.buttons = []
 
         opt = this.normaliseOptions(opt);
         this.setStyle(opt);
@@ -133,15 +133,15 @@ define(function (require, exports, module) {
     Pagination.prototype.createHTML = function () {
         /*** create the ul element */
         let ul = this.div.append('nav')
-                .attr('aria-label', this.ariaLabel)
-                .style('position','absolute')
-                .style('top', `${this.coords.top}px`)
-                .style('left', `${this.coords.left}px`)                    
-                .append('ul')
-                    .attr('class','pagination')
+            .attr('aria-label', this.ariaLabel)
+            .style('position', 'absolute')
+            .style('top', `${this.coords.top}px`)
+            .style('left', `${this.coords.left}px`)
+            .append('ul')
+            .attr('class', 'pagination')
 
         /*** create previous buttons html if there is previous button */
-        if(this.previousButton){
+        if (this.previousButton) {
             this.createOuttermostPage(ul, 'Previous', true)
         }
 
@@ -152,46 +152,46 @@ define(function (require, exports, module) {
         let numLeft = num
         let numRight = num
         /*** calculate the number of pages on left */
-        if((this.activeIndex + num) > total-1){
-            numLeft = num + (this.activeIndex + num) - (total-1)
+        if ((this.activeIndex + num) > total - 1) {
+            numLeft = num + (this.activeIndex + num) - (total - 1)
             numRight = total - 1 - this.activeIndex
         }
         /*** calculate the number of pages on right */
-        if((this.activeIndex - num) <= 1){
-            numLeft = this.activeIndex -2
-            numRight = 2*num - numLeft
+        if ((this.activeIndex - num) <= 1) {
+            numLeft = this.activeIndex - 2
+            numRight = 2 * num - numLeft
         }
         /*** for each page create a page while it fits on container width */
         this.pages.forEach((page, index) => {
-            index = index +1
+            index = index + 1
             /*** if the pages fits all inside container width */
-            if( this.pages.length < numButtons ){
+            if (this.pages.length < numButtons) {
                 this.createPage(ul, index, index)
-            }else{
+            } else {
                 /*** render first, last and then the inner pages that fits on widget width, create ... indicating ther are some more pages */
                 //render just the first, the last, the active one, ... after first and before last and then create how many buttons it fits
-                if(index === 1){
+                if (index === 1) {
                     this.createPage(ul, index, index)
-                    if(this.activeIndex - num > 2){
+                    if (this.activeIndex - num > 2) {
                         this.createPage(ul, '...', -1)
                     }
                 }
-                if(index === this.pages.length){
-                    if(this.activeIndex + num < this.pages.length -1){
+                if (index === this.pages.length) {
+                    if (this.activeIndex + num < this.pages.length - 1) {
                         this.createPage(ul, '...', -1)
                     }
-                    this.createPage(ul, index, index)   
+                    this.createPage(ul, index, index)
                 }
-                if(index >= (this.activeIndex - numLeft) && index <= (this.activeIndex + numRight)){
-                    if(index > 1 && index < this.pages.length){
+                if (index >= (this.activeIndex - numLeft) && index <= (this.activeIndex + numRight)) {
+                    if (index > 1 && index < this.pages.length) {
                         this.createPage(ul, index, index)
                     }
-                    
-                }                    
+
+                }
             }
         });
         /*** create next button if there is a next button */
-        if(this.nextButton){
+        if (this.nextButton) {
             this.createOuttermostPage(ul, 'Next', false)
         }
         this.position = 0;
@@ -211,37 +211,37 @@ define(function (require, exports, module) {
     Pagination.prototype.createPage = function (parent, title, index) {
         /*** create HTML */
         let li = parent.append('li')
-                    .attr('class','page-item')            
-        if(this.activeIndex === index){
-            li.attr('class','active')
+            .attr('class', 'page-item')
+        if (this.activeIndex === index) {
+            li.attr('class', 'active')
         }
         let x = li.append('a')
-            .attr('class','page-link')
-            .attr('href','#')
+            .attr('class', 'page-link')
+            .attr('href', '#')
             .html(title)
 
         /*** create ButtonEvo */
-        let element_coords =  x.node().getBoundingClientRect()
+        let element_coords = x.node().getBoundingClientRect()
         let found = false
-        found = this.buttons.find(function(element) {return element.id == index})
-        if(index !== -1 && !found){
+        found = this.buttons.find(function (element) { return element.id == index })
+        if (index !== -1 && !found) {
             this.buttons.push({
                 id: index,
                 button: new ButtonEVO(`${this.plainParent}_page_${index}`, {
                     width: element_coords.width,
                     height: element_coords.height,
-                    top: element_coords.top-20, // this 20 is the margin, but i can't figure out how to work it arround
+                    top: this.coords.top + 20, // this 20 is the margin, but i can't figure out how to work it arround
                     left: element_coords.left
                 }, {
-                    softLabel: "",
-                    backgroundColor: "steelblue",
-                    opacity: "0.2",
-                    borderRadius: "4px",
-                    fontsize: 34,
-                    parent: `${this.id}`,
-                    callback: this.callback,
-                    zIndex: 10
-                })
+                        softLabel: "",
+                        backgroundColor: "steelblue",
+                        opacity: "0.2",
+                        borderRadius: "4px",
+                        fontsize: 34,
+                        parent: `${this.id}`,
+                        callback: this.callback,
+                        zIndex: 10
+                    })
             })
         }
         return this
@@ -260,11 +260,11 @@ define(function (require, exports, module) {
         let numButtons = 0
         numButtons = Math.floor(this.coords.width / 35) //button width
         /*** remove one button if there is a previous button */
-        if(this.previousButton){
-            numButtons = numButtons - 1    
+        if (this.previousButton) {
+            numButtons = numButtons - 1
         }
         /*** remove one button if there is a next button */
-        if(this.nextButton){
+        if (this.nextButton) {
             numButtons = numButtons - 1
         }
         return numButtons
@@ -285,36 +285,36 @@ define(function (require, exports, module) {
     Pagination.prototype.createOuttermostPage = function (parent, title, isPrevious) {
         /*** create HTML */
         let li = parent.append('li')
-                .attr('class', 'page-item')
-                .append('a')
-                    .attr('class','page-link')
-                    .attr('href','#')
-                    .attr('aria-label',title)
-            if(this.useIcons){
-                li.append('span')
-                    .attr('aria-hidden','true')
-                    /*** if it is previous then « else is next, » */
-                    .html(isPrevious === true ? '&laquo;' : '&raquo;')
-                li.append('span')
-                    .attr('class','sr-only')
-                    .html(title)
-            }else{
-                li.html(title)
-            }
+            .attr('class', 'page-item')
+            .append('a')
+            .attr('class', 'page-link')
+            .attr('href', '#')
+            .attr('aria-label', title)
+        if (this.useIcons) {
+            li.append('span')
+                .attr('aria-hidden', 'true')
+                /*** if it is previous then « else is next, » */
+                .html(isPrevious === true ? '&laquo;' : '&raquo;')
+            li.append('span')
+                .attr('class', 'sr-only')
+                .html(title)
+        } else {
+            li.html(title)
+        }
 
-            /*** create ButtonEvo */
-            let element_coords =  li.node().getBoundingClientRect()
-            let buttonTitle = isPrevious ? 'previous' : 'next'
-            if(!this.hasButton(this.buttons, `${buttonTitle}`)){
-                this.buttons.push(
-                    {
-                        id: `${buttonTitle}`,
-                        button : new ButtonEVO(`${buttonTitle}_page`, {
-                            width: element_coords.width,
-                            height: element_coords.height,
-                            top: element_coords.top-20, // this 20 is the margin, but i can't figure out how to work it arround
-                            left: element_coords.left
-                        }, {
+        /*** create ButtonEvo */
+        let element_coords = li.node().getBoundingClientRect()
+        let buttonTitle = isPrevious ? 'previous' : 'next'
+        if (!this.hasButton(this.buttons, `${buttonTitle}`)) {
+            this.buttons.push(
+                {
+                    id: `${buttonTitle}`,
+                    button: new ButtonEVO(`${buttonTitle}_page`, {
+                        width: element_coords.width,
+                        height: element_coords.height,
+                        top: this.coords.top + 20, // element_coords.top - 20, // this 20 is the margin, but i can't figure out how to work it arround
+                        left: element_coords.left
+                    }, {
                             softLabel: "",
                             backgroundColor: "steelblue",
                             opacity: "0.2",
@@ -324,10 +324,10 @@ define(function (require, exports, module) {
                             callback: this.callback,
                             zIndex: 10
                         })
-                    }
-                )
-            }  
-            return this                  
+                }
+            )
+        }
+        return this
     }
 
     /**
@@ -342,7 +342,7 @@ define(function (require, exports, module) {
     */
     Pagination.prototype.hasButton = function (button_array, value) {
         let found
-        found = button_array.find(function(element) {return element.id === value})
+        found = button_array.find(function (element) { return element.id === value })
         return found !== undefined;
     }
 
@@ -358,7 +358,7 @@ define(function (require, exports, module) {
         this.activeIndex = page;
         this.render();
         return this;
-    } 
+    }
 
     /**
     * @function <a name="getActiveIndex">getActiveIndex</a>
@@ -380,7 +380,7 @@ define(function (require, exports, module) {
     */
     Pagination.prototype.getTotalPages = function () {
         return this.pages.length
-    }   
+    }
     module.exports = Pagination
-   }
+}
 )
