74f32a009e1e063d3a089cd1a75ce11591ba3ca7
Tiago Carvalheira <=>
2018-07-18 15:34:28 +0100

    Changed render function to accept and handle pvs state

--- src/client/app/widgets/med/DetailPanel/MaxMinDisplay.js
+++ src/client/app/widgets/med/DetailPanel/MaxMinDisplay.js
@@ -37,7 +37,8 @@
 define(function (require, exports, module) {
     "use strict";
     var WidgetEVO = require("widgets/core/WidgetEVO");
-    let BasicDisplay = require('widgets/core/BasicDisplayEVO')
+    const BasicDisplay = require('widgets/core/BasicDisplayEVO')
+    const d3 = require("d3/d3");
     /**
      * @function <a name="MaxMinDisplay">MaxMinDisplay</a>
      * @description Constructor. Create a MaxMinDisplay, extending WidgetEVO
@@ -57,6 +58,11 @@ define(function (require, exports, module) {
      * @param {('Integer'|'Float'|'String')} [opt.type='Float'] set widget type. 
      * @param {Integer} [decimalPlaces=1] Set the number of decimal places
      * @param {('none' | 'parenthesis' | 'bracked' | 'curly')} [bracket='none'] options are  default is none
+     * @param {String} [opt.pvsMinValue='minValue']
+     * @param {String} [opt.pvsMaxValue='maxValue']
+     * @param {String} [opt.pvsValue='value']
+     * @param {String} [opt.pvsTitle='title]
+     * @param {String} [opt.pvsSubtitle='subtitle']
      * @memberof module:MaxMinDisplay
      * @instance
      */
@@ -66,7 +72,17 @@ define(function (require, exports, module) {
          opt = this.normaliseOptions(opt);
          // set widget type & display key
          this.type = this.type || "MaxMinDisplay";
-         this.displayKey = (typeof opt.displayKey === "string") ? opt.displayKey : id;
+        this.displayKey = (typeof opt.displayKey === "string") ? opt.displayKey : id;
+        this.pvsValue = opt.pvsValue || this.pvsValue || 'value'
+        this.pvsMinValue = opt.pvsMinValue || this.pvsMinValue || 'minValue'
+        this.pvsMaxValue = opt.pvsMaxValue || this.pvsMaxValue || 'maxValue'
+        this.pvsTitle = opt.pvsTitle || this.pvsTitle || 'title'
+        this.pvsSubTitle = opt.pvsSubTitle || this.pvsSubtitle || 'subtitle'
+        this.parent = (opt.parent) ? (`#${opt.parent}`) : 'body'
+        this.div = d3.select(this.parent)
+            .append('div')
+                .attr('id', `${id}_div`)
+                .style('display','block')
 
         opt.backgroundColor = opt.backgroundColor || "black";
         opt.cursor = opt.cursor || "default";
@@ -76,62 +92,82 @@ define(function (require, exports, module) {
         
          // invoke WidgetEVO constructor to create the widget
         WidgetEVO.apply(this, [ id, coords, opt ]);
-        let top = coords.top - 20
+        let top = 0
         if(this.title !== ''){
             let coordsTitle = Object.assign({},coords)
             coordsTitle.top = top
+            coordsTitle.left = 0
+            
             let optTitle = Object.assign({},opt)
+            optTitle.parent = this.div.node().id
+            optTitle.position = 'relative'
             optTitle.fontSize = '8'
             optTitle.opacity = '1'
             optTitle.backgroundColor = 'rgba(0, 0, 0, 0)'
             optTitle.fontWeight = '900'
-            this.titleDisplay = new BasicDisplay(id+'_titledisplay', coordsTitle, optTitle)
+            this.titleDisplay = new BasicDisplay(`${id}_titledisplay`, coordsTitle, optTitle)
             top = top+10
         }
 
         if(this.subtitle !== ''){
             let coordsSubTitle = Object.assign({},coords)
+            //coordsSubTitle.top = top
             coordsSubTitle.top = top
+            coordsSubTitle.left = 0
             let optSubTitle = Object.assign({},opt)
+            optSubTitle.parent = this.div.node().id
+            optSubTitle.position = 'relative'
             optSubTitle.fontSize = '6'
             optSubTitle.opacity = '1'
             optSubTitle.backgroundColor = 'rgba(0, 0, 0, 0)'
             optSubTitle.fontWeight = '900'
-            this.subTitleDisplay = new BasicDisplay(id+'_subtitledisplay', coordsSubTitle, optSubTitle)
+            this.subTitleDisplay = new BasicDisplay(`${id}_subtitledisplay`, coordsSubTitle, optSubTitle)
             top = top+10
         }
 
 
         if(this.valueMin != ''){
             let coordsMin = Object.assign({},coords)
+            //coordsMin.top = top
             coordsMin.top = top
+            coordsMin.left = 0
             let optMin = Object.assign({},opt)
+            optMin.parent = this.div.node().id
+            optMin.position = 'relative'
             optMin.fontSize = '8'
             optMin.opacity = '0.6',
             optMin.align = 'right'
             optMin.backgroundColor = 'rgba(0, 0, 0, 0)'
-            this.tMinDisplay = new BasicDisplay(id+'_tmindisplay', coordsMin, optMin)
+            this.tMinDisplay = new BasicDisplay(`${id}_tmindisplay`, coordsMin, optMin)
             top = top +10
         }
         
         if(this.valueMax !== ''){
             let coordsMax = Object.assign({},coords)
+            //coordsMax.top = top
             coordsMax.top = top
+            coordsMax.left = 0
             let optMax = Object.assign({},opt)
+            optMax.parent = this.div.node().id
+            optMax.position = 'relative'
             optMax.fontSize = '8'
             optMax.opacity = '0.6'
             optMax.backgroundColor = 'rgba(0, 0, 0, 0)'
-            this.tMaxDisplay = new BasicDisplay(id+'_tmaxdisplay', coordsMax, optMax)
+            this.tMaxDisplay = new BasicDisplay(`${id}_tmaxdisplay`, coordsMax, optMax)
         }
         
 
         let coordsTemp = coords
         let optTemp = opt
-        coordsTemp.top = coords.top
-        coordsTemp.left = coords.left + 30
+        //coordsTemp.top = coords.top
+        //coordsTemp.left = coords.left + 30
+        coordsTemp.top = 0
+        coordsTemp.left = 30
+        optTemp.parent = this.div.node().id
+        optTemp.position = 'relative'
         optTemp.backgroundColor = 'rgba(0,0,0,0)'
         optTemp.fontSize = opt.fontSize
-        this.tempDisplay = new BasicDisplay(id+'_tdisplay', coordsTemp, optTemp)
+        this.tempDisplay = new BasicDisplay(`${id}_tdisplay`, coordsTemp, optTemp)
         
 
         return this;
@@ -144,15 +180,44 @@ define(function (require, exports, module) {
       * @function <a name="render">render</a>
       * @description Rendering function for button widgets.
       * @param state {Object} JSON object with the current value of the state attributes of the modelled system
-      * @param opt {Object} Style options overriding the style attributes used when the widget was created.
+      * @param {Object} opt Style options overriding the style attributes used when the widget was created.
       *                     The override style options are temporary, i.e., they are applied only for the present invocation of the render method.
       *                     Available options are either html style attributes or the following widget attributes:
+      * @param {String} [opt.pvsMinValue='minValue']
+      * @param {String} [opt.pvsMaxValue='maxValue']
+      * @param {String} [opt.pvsValue='value']
+      * @param {('Integer'|'Float'|'String')} [opt.type='Float'] set widget type. 
+      * @param {Integer} [opt.decimalPlaces=1] Set the number of decimal places
+      * @param {('none' | 'parenthesis' | 'bracked' | 'curly')} [opt.bracket='none'] options are  default is none
+      * * @param {String} [opt.pvsTitle='title]
+     * @param {String} [opt.pvsSubtitle='subtitle']
       * @memberof module:MaxMinDisplay
       * @instance
       */
      MaxMinDisplay.prototype.render = function (state, opt) {
          // set style
          opt = this.normaliseOptions(opt);
+
+        this.pvsValue = opt.pvsValue || this.pvsValue || 'value'
+        this.pvsMinValue = opt.pvsMinValue || this.pvsMinValue || 'minValue'
+        this.pvsMaxValue = opt.pvsMaxValue || this.pvsMaxValue || 'maxValue'
+        this.decimalPlaces = opt.decimalPlaces || this.decimalPlaces || 1
+        this.bracket = opt.bracket || this.bracket || 'none'
+        this.type = opt.type || this.type || 'Float'
+        this.pvsTitle = opt.pvsTitle || this.pvsTitle || 'title'
+        this.pvsSubTitle = opt.pvsSubtitle || this.pvsSubTitle || 'subtitle'
+         // if state is just a number then it will be the   main value. 
+        if(typeof state === 'number'){
+            this.value = state
+        }else{  
+            // if it is an object, check for min value, max value, and value with options is possible to define new state names to this variables
+            this.value = state[this.pvsValue] || this.value || 0
+            this.valueMin = state[this.pvsMinValue] || this.valueMin || 0 
+            this.valueMax = state[this.pvsMaxValue] || this.valueMax || 0
+        }
+
+        this.title = state[this.pvsTitle] || this.title  || ''
+        this.subtitle = state[this.pvsSubTitle] || this.subtitle || ''
 
         this.setStyle(opt);
 
@@ -210,6 +275,18 @@ define(function (require, exports, module) {
         this.tempDisplay.render(`${openBrackets}${val}${closeBrackets}`, {align: 'left'})
         this.reveal();
         return this;
+     }
+
+     	/**
+         * @function <a name="hide">hide</a>
+         * @description  Method that will hide display when called
+         * @memberof module:MaxMinDisplay
+         * @return returns self component to be able to call it in a chained mode
+         * @instance
+         */
+         MaxMinDisplay.prototype.hide = function () {
+            this.div.style('display' , 'none')
+            return this
      }
 
      	/**
